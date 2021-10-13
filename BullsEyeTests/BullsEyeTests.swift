69634c576d3567b1f8c9941b37d096eeb6b66817
Albert Pangestu <stamina.left@gmail.com>
2021-10-13 15:31:51 +0700

    Created System Under Test (SUT) placeholder with type of BullsEyeGame.

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -30,6 +30,8 @@ import XCTest
 @testable import BullsEye
 
 class BullsEyeTests: XCTestCase {
+  
+  var sut: BullsEyeGame!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
