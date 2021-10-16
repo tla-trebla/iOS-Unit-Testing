73d6ebb064a9f207c268a8a58106b29ba40a5b61
Albert Pangestu <stamina.left@gmail.com>
2021-10-16 17:00:08 +0700

    Created new SUT with type URLSession.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -31,6 +31,8 @@ import XCTest
 
 class BullsEyeSlowTests: XCTestCase {
 
+  var sut: URLSession!
+  
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
