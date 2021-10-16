1900b5e33376d908523dd8ab7de32b32ca8213fb
Albert Pangestu <stamina.left@gmail.com>
2021-10-16 18:26:40 +0700

    Set SUT's URLSession configuration with 'default' so it'll able to incrementally delegate.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -34,7 +34,8 @@ class BullsEyeSlowTests: XCTestCase {
   var sut: URLSession!
   
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+      sut = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
