5b45d723522ae3d781bf3eb5b469dc2834a80966
Albert Pangestu <stamina.left@gmail.com>
2021-10-16 22:48:16 +0700

    Deleted unused test.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -33,26 +33,15 @@ class BullsEyeSlowTests: XCTestCase {
 
   var sut: URLSession!
   
-    override func setUpWithError() throws {
-      try super.setUpWithError()
-      sut = URLSession(configuration: .default)
-    }
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = URLSession(configuration: .default)
+  }
 
-    override func tearDownWithError() throws {
-      sut = nil
-      try super.tearDownWithError()
-    }
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
