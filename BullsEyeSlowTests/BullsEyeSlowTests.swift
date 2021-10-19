1e4f3a6b0fa1289589271729fed502d0a2768013
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 19:11:59 +0700

    Placed Network Monitor initialization into a global field.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -31,15 +31,14 @@ import XCTest
 
 class BullsEyeSlowTests: XCTestCase {
 
-    var sut: URLSession!
+  var sut: URLSession!
+  let networkMonitor = NetworkMonitor.shared
   
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = URLSession(configuration: .default)
-        let networkMonitor = NetworkMonitor.shared
     }
 
-  
     override func tearDownWithError() throws {
         sut = nil
         try super.tearDownWithError()
@@ -47,31 +46,31 @@ class BullsEyeSlowTests: XCTestCase {
 
     // Asynchronous test: success fast, failure slow
     func testValidApiCallGetsHTTPStatusCode200() throws {
-    
-        // given
-        let urlString = "https://www.boredapi.com/api/activity"
-        let url = URL(string: urlString)!
-        // 1
-        let promise = expectation(description: "Status code: 200")
-    
-        // when
-        let dataTask = sut.dataTask(with: url) { _, response, error in
-            // then
-            if let error = error {
-                XCTFail("Error: \(error.localizedDescription)")
-                return
-            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                if statusCode == 200 {
-                    // 2
-                    promise.fulfill()
-                } else {
-                    XCTFail("Status code: \(statusCode)")
-                }
-            }
-        }
-        dataTask.resume()
-        // 3
-        wait(for: [promise], timeout: 5)
+      
+      // given
+      let urlString = "https://www.boredapi.com/api/activity"
+      let url = URL(string: urlString)!
+      // 1
+      let promise = expectation(description: "Status code: 200")
+  
+      // when
+      let dataTask = sut.dataTask(with: url) { _, response, error in
+          // then
+          if let error = error {
+              XCTFail("Error: \(error.localizedDescription)")
+              return
+          } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+              if statusCode == 200 {
+                  // 2
+                  promise.fulfill()
+              } else {
+                  XCTFail("Status code: \(statusCode)")
+              }
+          }
+      }
+      dataTask.resume()
+      // 3
+      wait(for: [promise], timeout: 5)
     }
   
     func testApiCallCompletes() throws {
