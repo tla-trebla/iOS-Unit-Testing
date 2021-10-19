7854f45559b5b2dc55322b26df0558bede172ce3
Albert Pangestu <stamina.left@gmail.com>
2021-10-20 00:03:24 +0700

    When a test encounter a failure, don't continue.

--- BullsEyeUITests/BullsEyeUITests.swift
+++ BullsEyeUITests/BullsEyeUITests.swift
@@ -32,14 +32,10 @@ class BullsEyeUITests: XCTestCase {
   
   var app: XCUIApplication!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+  }
 
     func testExample() throws {
         // UI tests must launch the application that they test.
