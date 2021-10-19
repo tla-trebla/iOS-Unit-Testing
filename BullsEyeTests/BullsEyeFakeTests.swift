3b481bdb0769ebd0098b6c2f020c2e7e9ef37a5a
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 19:37:55 +0700

    Initialized SUT.

--- BullsEyeTests/BullsEyeFakeTests.swift
+++ BullsEyeTests/BullsEyeFakeTests.swift
@@ -34,7 +34,8 @@ class BullsEyeFakeTests: XCTestCase {
   var sut: BullsEyeGame!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+      sut = BullsEyeGame()
     }
 
     override func tearDownWithError() throws {
