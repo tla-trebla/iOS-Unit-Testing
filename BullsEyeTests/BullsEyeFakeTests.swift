83f65d3f860d162ad62206e7eb3357c5f4ed75d6
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 19:38:28 +0700

    Set SUT to nil for each test.

--- BullsEyeTests/BullsEyeFakeTests.swift
+++ BullsEyeTests/BullsEyeFakeTests.swift
@@ -39,7 +39,8 @@ class BullsEyeFakeTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+      sut = nil
+      try super.tearDownWithError()
     }
 
     func testExample() throws {
