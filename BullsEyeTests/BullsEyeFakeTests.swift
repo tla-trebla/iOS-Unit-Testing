31a7ac787a12bdfecc4633ab327c918303c11674
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 19:42:48 +0700

    Edited indentation.

--- BullsEyeTests/BullsEyeFakeTests.swift
+++ BullsEyeTests/BullsEyeFakeTests.swift
@@ -33,26 +33,14 @@ class BullsEyeFakeTests: XCTestCase {
   
   var sut: BullsEyeGame!
 
-    override func setUpWithError() throws {
-      try super.setUpWithError()
-      sut = BullsEyeGame()
-    }
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = BullsEyeGame()
+  }
 
-    override func tearDownWithError() throws {
-      sut = nil
-      try super.tearDownWithError()
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+  
 }
