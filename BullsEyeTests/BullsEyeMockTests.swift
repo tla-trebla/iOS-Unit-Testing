70ce8b1e50b4bec955b4a4431a0118a54ad00594
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 22:41:44 +0700

    Deleted unused tests.

--- BullsEyeTests/BullsEyeMockTests.swift
+++ BullsEyeTests/BullsEyeMockTests.swift
@@ -55,17 +55,4 @@ class BullsEyeMockTests: XCTestCase {
     mockUserDefaults = nil
     try super.tearDownWithError()
   }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
