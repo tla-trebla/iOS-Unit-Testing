ab7765fb0757aaafe03bde5e73df6981ce3db41c
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 19:07:35 +0700

    Edited indentation.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -31,67 +31,69 @@ import XCTest
 
 class BullsEyeSlowTests: XCTestCase {
 
-  var sut: URLSession!
+    var sut: URLSession!
   
-  override func setUpWithError() throws {
-      try super.setUpWithError()
-      sut = URLSession(configuration: .default)
-      let networkMonitor = NetworkMonitor.shared
-  }
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = URLSession(configuration: .default)
+        let networkMonitor = NetworkMonitor.shared
+    }
 
-  override func tearDownWithError() throws {
-    sut = nil
-    try super.tearDownWithError()
-  }
+  
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
 
-  // Asynchronous test: success fast, failure slow
-  func testValidApiCallGetsHTTPStatusCode200() throws {
-    // given
-    let urlString = "https://www.boredapi.com/api/activity"
-    let url = URL(string: urlString)!
-    // 1
-    let promise = expectation(description: "Status code: 200")
+    // Asynchronous test: success fast, failure slow
+    func testValidApiCallGetsHTTPStatusCode200() throws {
     
-    // when
-    let dataTask = sut.dataTask(with: url) { _, response, error in
-      // then
-      if let error = error {
-        XCTFail("Error: \(error.localizedDescription)")
-        return
-      } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-        if statusCode == 200 {
-          // 2
-          promise.fulfill()
-        } else {
-          XCTFail("Status code: \(statusCode)")
+        // given
+        let urlString = "https://www.boredapi.com/api/activity"
+        let url = URL(string: urlString)!
+        // 1
+        let promise = expectation(description: "Status code: 200")
+    
+        // when
+        let dataTask = sut.dataTask(with: url) { _, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
-      }
+        dataTask.resume()
+        // 3
+        wait(for: [promise], timeout: 5)
     }
-    dataTask.resume()
-    // 3
-    wait(for: [promise], timeout: 5)
-  }
   
-  func testApiCallCompletes() throws {
-    // given
-    let urlString = "https://www.boredapi.com/api/test"
-    let url = URL(string: urlString)!
-    let promise = expectation(description: "Completion handler invoked")
-    var statusCode: Int?
-    var responseError: Error?
+    func testApiCallCompletes() throws {
+        // given
+        let urlString = "https://www.boredapi.com/api/test"
+        let url = URL(string: urlString)!
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
     
-    // when
-    let dataTask = sut.dataTask(with: url) { _, response, error in
-      statusCode = (response as? HTTPURLResponse)?.statusCode
-      responseError = error
-      promise.fulfill()
-    }
-    dataTask.resume()
-    wait(for: [promise], timeout: 5)
+        // when
+        let dataTask = sut.dataTask(with: url) { _, response, error in
+          statusCode = (response as? HTTPURLResponse)?.statusCode
+          responseError = error
+          promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
     
-    // then
-    XCTAssertNil(responseError)
-    XCTAssertEqual(statusCode, 200)
-  }
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+      }
 
 }
