0950d99202531c0e8d535e5c178aee55f74f8996
Albert Pangestu <stamina.left@gmail.com>
2021-10-19 22:26:53 +0700

    Edited setupWithError indentation.

--- BullsEyeTests/BullsEyeMockTests.swift
+++ BullsEyeTests/BullsEyeMockTests.swift
@@ -43,9 +43,9 @@ class BullsEyeMockTests: XCTestCase {
   var sut: ViewController!
   var mockUserDefaults: MockUserDefaults!
   
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    
+  }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
