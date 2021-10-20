e3e67e731cf7fc88848429d0b2f37601250c531e
Albert Pangestu <stamina.left@gmail.com>
2021-10-20 20:47:52 +0700

    Deleted tap function at the slideButton.

--- BullsEyeUITests/BullsEyeUITests.swift
+++ BullsEyeUITests/BullsEyeUITests.swift
@@ -42,7 +42,6 @@ class BullsEyeUITests: XCTestCase {
   func testGameStyleSwitch() {
     // given
     let slideButton = app/*@START_MENU_TOKEN@*/.segmentedControls.buttons["Slide"]/*[[".segmentedControls.buttons[\"Slide\"]",".buttons[\"Slide\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-    slideButton.tap()
     app/*@START_MENU_TOKEN@*/.buttons["Type"]/*[[".segmentedControls.buttons[\"Type\"]",".buttons[\"Type\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     app.staticTexts["Guess where the slider is: "].tap()
     slideButton.tap()
